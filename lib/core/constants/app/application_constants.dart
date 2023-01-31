84ad710fd07dcd0bc45164fc7df12f1320292c81
Berkay-Ates <atesberkay2356@gmail.com>
2023-01-31 23:43:51 +0300

    last of lasts commits

---
+++ lib/core/constants/app/application_constants.dart
@@ -0,0 +1,7 @@
+class ApplicationConstants {
+  static const langAssetPath = 'asset/lang';
+  static const EmailRegex = r"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$";
+  static const REGISTER_USER_URL = '';
+  static const LOGIN_USER_URL = '';
+  static const USER_ALL_QR_CODES = ''; // api keys endpoints you should provide an endpoint by yourself
+}
