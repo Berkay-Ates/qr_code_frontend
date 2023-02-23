646b747e13fa9e0cc48e7be1c553afd6cccf83f6
Berkay-Ates <atesberkay2356@gmail.com>
2023-02-23 15:07:05 +0300

    last changes done

--- lib/core/constants/app/application_constants.dart
+++ lib/core/constants/app/application_constants.dart
@@ -1,12 +1,14 @@
 class ApplicationConstants {
   static const langAssetPath = 'asset/lang';
   static const EmailRegex = r"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$";
-  static const QR_SAVE_URL = 'https://qr-code-gen.onrender.com/api/v1/qr/saveQRCode';
-  static const QR_DELETE_URL = 'https://qr-code-gen.onrender.com/api/v1/qr/';
-  static const REGISTER_USER_URL = 'https://qr-code-gen.onrender.com/api/v1/auth/register';
-  static const LOGIN_USER_URL = 'https://qr-code-gen.onrender.com/api/v1/auth/login';
-  static const USER_ALL_QR_CODES = 'https://qr-code-gen.onrender.com/api/v1/qr/getAllQRCode';
-  static const USER_ALL_SCANNED_QR_CODES = 'https://qr-code-gen.onrender.com/api/v1/qr/getAllScannedQRCode';
-  static const USER_ALL_GENERATED_QR_CODES = 'https://qr-code-gen.onrender.com/api/v1/qr/getAllGeneratedQRCode';
-  static const FIREBASE_BASE_URL = "https://firebasestorage.googleapis.com/v0/b/clash-cards-77641.appspot.com/o";
+  static const QR_SAVE_URL = '';
+  static const QR_DELETE_URL = '';
+  static const REGISTER_USER_URL = '';
+  static const LOGIN_USER_URL = '';
+  static const USER_ALL_QR_CODES = '';
+  static const USER_ALL_SCANNED_QR_CODES = '';
+  static const USER_ALL_GENERATED_QR_CODES = '';
+  static const FIREBASE_BASE_URL = "";
+
+  //* you have to add those endpoint yourself
 }
